ee4011e5c5d6615543e1ce7281cfb99fdac292af
Greg Blair <GregBlair@msn.com>
2021-03-14 18:47:48 -0500

    use constants

--- Button Test.py
+++ Button Test.py
@@ -2,21 +2,22 @@ import time
 # import random
 import RPi.GPIO as GPIO
 # import pygame
+import constants as CS 
 
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BOARD)
 # GPIO.setmode( GPIO.BCM)
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(CS.GPIO_OUTPUT_PIN, GPIO.OUT)
+GPIO.setup(CS.GPIO_INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True: 
     print("Testing now!")
-    if GPIO.input(12) == GPIO.HIGH:
+    if GPIO.input(CS.GPIO_INPUT_PIN) == GPIO.HIGH:
         print("Button was pushed!")
-        GPIO.output(16, GPIO.HIGH)
+        GPIO.output(CS.GPIO_OUTPUT_PIN, GPIO.HIGH)
         print("Sleeping for 3!")
         time.sleep(3)
-        GPIO.output(16, GPIO.LOW)
+        GPIO.output(CS.GPIO_OUTPUT_PIN, GPIO.LOW)
     
         
 
