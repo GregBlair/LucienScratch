23ae46e81d09c368ae88d645b68ba6931b266d33
Lucien <gblair585@gmail.com>
2021-03-27 18:34:04 -0500

    meets current requirements

--- nurse call botton.py
+++ nurse call botton.py
@@ -10,23 +10,29 @@ GPIO.setup(CS.GPIO_NURSE_OUTPUT_PIN, GPIO.OUT)
 GPIO.setup(CS.GPIO_PATIENT_INPUT_PIN, GPIO.IN)
 GPIO.setup(CS.GPIO_NURSE_INPUT_PIN, GPIO.IN)
 
+STATE=CS.WAITING
 
 def isPatietButtonPushed():
-        number=random.randint(0,12)
+        number=random.randint(1,10)
         print(number)
-        return number<6
+        return number<9
 
 while True:
-        if isPatietButtonPushed():
+            if isPatietButtonPushed():
+            STATE=CS.WAITING
             print('patient button has been pushed ')
             GPIO.output(CS.GPIO_PATIENT_OUTPUT_PIN, GPIO.HIGH)
             GPIO.output(CS.GPIO_NURSE_OUTPUT_PIN, GPIO.HIGH)
-                        
-            time.sleep(CS.LIGHT_DELAY)
+                    
 
         else:
             print('patient button has not been pushed')
             GPIO.output(CS.GPIO_NURSE_OUTPUT_PIN, GPIO.LOW)
             GPIO.output(CS.GPIO_PATIENT_OUTPUT_PIN, GPIO.LOW)
+            STATE=CS.WAITING
+        
+        def PLAY_AMARM_SOUND
+            if STATE=CS.WAITING
+            play
             
-        time.sleep(CS.LIGHT_DELAY)       
+        time.sleep=(CS.LIGHT_DELAY)
