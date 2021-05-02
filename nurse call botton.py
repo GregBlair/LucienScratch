41b9ed529bdf23dec639d7ed5844596aca0115eb
Lucien <gblair585@gmail.com>
2021-05-01 19:11:07 -0500

    events on both buttons

--- nurse call botton.py
+++ nurse call botton.py
@@ -17,9 +17,16 @@ GPIO.setup(CS.GPIO_NURSE_OUTPUT_PIN, GPIO.OUT)
 
 
 patientButtonPushed = False
+nurseButtonPushed = False 
 def patient_button_callback(channel):
     print("Patient button callback!")
-    global patientButtonPushed = True
+    global patientButtonPushed
+    patientButtonPushed = True
+    
+def nurse_button_callback(channel):
+    print("Nurse button callback!")
+    global nurseButtonPushed
+    nurseButtonPushed = True
     
 def playAlarmSound():
     GPIO.output(CS.GPIO_NURSE_OUTPUT_PIN, GPIO.HIGH)
@@ -35,21 +42,27 @@ def isPatietButtonPushed():
     return patientButtonPushed
 
 def isNurseButtonPushed():
-    return GPIO.input(CS.GPIO_NURSE_INPUT_PIN) == GPIO.HIGH
+    return nurseButtonPushed
 
 GPIO.add_event_detect(CS.GPIO_PATIENT_INPUT_PIN,
                       GPIO.RISING,
                       callback=patient_button_callback) # Setup event on patient button pin rising edge
 
+GPIO.add_event_detect(CS.GPIO_NURSE_INPUT_PIN,
+                      GPIO.RISING,
+                      callback=nurse_button_callback) 
 STATE=CS.IDLE
-
+PatientButtonPushed = False
 while True:
     if STATE==CS.IDLE:
         if isPatietButtonPushed():
             STATE=CS.WAITING
             print('patient button has been pushed ')
             GPIO.output(CS.GPIO_PATIENT_OUTPUT_PIN, GPIO.HIGH)
-      
+#             PatientButtonPushed = False
+#             global patientButtonPushed
+            patientButtonPushed = False
+            print(patientButtonPushed) 
                     
 
     elif STATE==CS.WAITING:
@@ -58,6 +71,8 @@ while True:
             
             GPIO.output(CS.GPIO_PATIENT_OUTPUT_PIN, GPIO.LOW)
             STATE=CS.IDLE
+            nurseButtonPushed = False
+            
     
         else:
             playAlarmSound()
